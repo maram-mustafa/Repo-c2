331364e1420e36ab85c01cb69eccdd786f9c7a7f
maram mustafa <maram.w.mustafa@gmail.com>
2021-04-04 16:53:49 +0300

    add files

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let userName=prompt('Please Enter Your Name!');
+console.log(userName);
+
+let country=prompt('Please Enter Your Country of Residence!');
+console.log(country);
+
+let email=promot('please Enter Your Email!');
+console.log(email);
+
+let message=prompt('please leave a brief message you want to tell us about it ')
+console.log(message);
+
+alert(' your name is' + userName + ' your email -> ' + email + ' you live in ' + ' this message you leave it to us ' + message );
