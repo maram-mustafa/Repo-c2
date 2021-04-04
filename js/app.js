b0d4a126df354ffc5f7a300ca7adc52659382711
maram mustafa <maram.w.mustafa@gmail.com>
2021-04-04 17:11:57 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 'use strict';
 
 let userName=prompt('Please Enter Your Name!');
-console.log(userName);
+console.log(typeof userName);
 
 let country=prompt('Please Enter Your Country of Residence!');
-console.log(country);
+console.log(typeof country);
 
-let email=promot('please Enter Your Email!');
-console.log(email);
+let email=prompt('please enter your email!');
+console.log(typeof email);
 
-let message=prompt('please leave a brief message you want to tell us about it ')
-console.log(message);
+let message=prompt('please leave a message for us');
+console.log(typeof message);
 
-alert(' your name is' + userName + ' your email -> ' + email + ' you live in ' + ' this message you leave it to us ' + message );
+alert(' your name is ' + userName + ' your email ->  ' + email + ' you live in ' + ' this message you leave it to us ' + message );
+
